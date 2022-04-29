66d84a0c4aa9415f9934eb21e591d50de8004dff
WonJuna <wja0810@naver.com>
2022-04-29 21:55:29 +0900

    고양이

---
+++ study144.java
@@ -0,0 +1,13 @@
+
+public class study144 {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("\\    /\\");
+		System.out.println(" )  ( ')");
+		System.out.println("(  /  )");
+		System.out.println(" \\(__)|");
+		
+	}
+
+}
